a69b6e173eb22ee7076fb6c79ba9bf774f0c0769
Brian Sellers <bsellers3013@gmail.com>
2023-08-07 19:27:26 -0500

    navbar almost there

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import Logo from '../assets/bslogo.png';
 
 const Navbar = () => {
+    const [show, setShow] = useState(false);
+    const handleClick = () => setShow(!show);
     return (
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-slate-900 text-white'>
+            
             <div>
                 <img src={Logo} alt='logo' className='w-[40px] h-[40px]' />
             </div>
-            <div>
-                <ul className='flex'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Projects</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-            <div className='hidden'>
-                <FaBars />
-            </div>
 
-            <ul className='hidden'>
+            <ul className='hidden md:flex'>
                 <li>Home</li>
                 <li>About</li>
                 <li>Projects</li>
                 <li>Contact</li>
             </ul>
 
-            <div>
+            <div onClick={handleClick} className='md:hidden z-10'>
+                <FaBars />
+            </div>
+            
+            <ul className={!show ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-slate-900 flex flex-col justify-center items-center'}>
+                <li className='py-6 text-4xl'>Home</li>
+                <li className='py-6 text-4xl'>About</li>
+                <li className='py-6 text-4xl'>Projects</li>
+                <li className='py-6 text-4xl'>Contact</li>
+            </ul>
+
+            <div className='hidden'>
 
             </div>
         </div>
