5443aa42499852514bd362942a899f40b7381f6c
Brian Sellers <bsellers3013@gmail.com>
2023-08-07 19:13:31 -0500

    sections for home, about, projects, contact

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,35 @@ import { FaBars, FaTimes } from 'react-icons/fa';
 import Logo from '../assets/bslogo.png';
 
 const Navbar = () => {
-  return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-slate-900 text-white'>
-      <div>
-        <img src={Logo} alt='logo' className='w-[40px] h-[40px]' />
-      </div>
-    </div>
-  )
+    return (
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-slate-900 text-white'>
+            <div>
+                <img src={Logo} alt='logo' className='w-[40px] h-[40px]' />
+            </div>
+            <div>
+                <ul className='flex'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className='hidden'>
+                <FaBars />
+            </div>
+
+            <ul className='hidden'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+
+            <div>
+
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
