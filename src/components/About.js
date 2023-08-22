9c9d438a9181fcb0c35c637657f63cfc907878bb
Brian Sellers <bsellers3013@gmail.com>
2023-08-22 01:47:50 -0500

    small changes

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,12 @@ const About = () => {
                 <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
                     <div className="sm:text-right pb-8 pl-4">
                         <p className="text-4xl font-bold inline border-b-4 border-[#fde047]">
-                            About
+                            About Me
                         </p>
                     </div>
                     <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
                         <div className='sm:text-right text-4xl font-bold'>
-                            <p>Hello, I am Brian Sellers Jr, take a look around.</p>
+                            <p></p>
                         </div>
                         <div>
                             <p>
